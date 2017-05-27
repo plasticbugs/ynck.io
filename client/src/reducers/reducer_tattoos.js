3a970af7dd06c840f2de700b832072ab8519ab8a
Scott Moschella <scottmosch@gmail.com>
2017-05-26 20:01:06 -0700

    Add barebones reducer for tattoo list container

---
+++ client/src/reducers/reducer_tattoos.js
@@ -0,0 +1,11 @@
+getTattoos = () => {
+  return [
+    { url: "https://s-media-cache-ak0.pinimg.com/736x/1e/99/84/1e99842826e3095f7b3db79c7ab16a3d.jpg" },
+    { url: "http://tattoosde.zonster.com/images/2/knochel/knochel-tattoo-design-204.jpg" },
+    { url: "https://laughingsquid.com/wp-content/uploads/2014/01/2565176.jpg" },
+    { url: "http://cdn.evoke.ie/2015/06/feature12-640x457.jpg" },
+    { url: "http://tattoocom.s3.amazonaws.com/585x375/funny_tattoos.jpg?itok=XEXrgYyU" }
+  ];
+};
+
+module.exports = getTattoos;
